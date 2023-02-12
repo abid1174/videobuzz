79be4d3ca1732ae3c3f46170d971a45fa004d181
Abid Al Amin <abidalamin9@gmail.com>
2023-02-12 16:55:59 +0600

    video file upload setup with multer

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import multer from "multer";
+
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const upload = multer({ dest: "uploads/" });
 
+app.post("/upload", upload.single("file"), uploadFiles);
+
+function uploadFiles(req, res) {
+  console.log(req.body);
+  console.log(req.file);
+  res.json({ message: "Successfully uploaded file" });
+}
+
+const port = 8000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
